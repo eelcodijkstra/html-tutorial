b74de594dbab54bcb683ac8621ff67911b842093
Eelco Dijkstra <eelco.j.dijkstra@gmail.com>
2014-11-13 15:19:50 +0100

    Use of FP-coordinates in turtle library

--- pjs-0/pjs0-gist2.js
+++ pjs-0/pjs0-gist2.js
@@ -1,23 +1,30 @@
+function log(s) {
+  var logField = document.getElementById("logArea");
+  var val = logField.value;
+  val = val + s + "\n";
+  logField.value = val;
+}
+
+function clearLog() {
+  var logField = document.getElementById("logArea");
+  logField.value = "// log \n";
+}
 
 var ctx; // the global context for the used Canvas element
 
-function initCanvas () {
+function initCanvas() {
   var canvas = document.getElementById("mycanvas");
   if (canvas.getContext) {
     ctx = canvas.getContext("2d");
   } else {
-    alert ("no canvas found");
+    alert("no canvas found");
   }
 }
 
 // turtle graphics variables
-var down=false;          // pen down
-var dir=0;               // current drawing direction (360 degrees? )
+var down = false;        // pen down
+var dir = 0;             // current drawing direction (360 degrees? )
 var pos = {x: 0, y: 0};  // current position
-
-function toPosition (x, y) {
-  pos.x = x; pos.y = y;
-}
 
 // sin and cos in degrees
 
@@ -30,28 +37,32 @@ function cos(d) {
 }
 
 function forward(n) {
-  var dx = Math.round(n * cos(dir));
-  var dy = Math.round(n * sin(dir));
-  var newpos = {x: pos.x+dx, y: pos.y+dy};
+  log("forward(" + n + ");");
+  var dx = n * cos(dir);
+  var dy = n * sin(dir);
+  var newpos = {x: pos.x + dx, y: pos.y + dy};
   if (down) {
-    ctx.beginPath ();
-    ctx.moveTo (pos.x, pos.y);
-    ctx.lineTo (newpos.x, newpos.y);
-    ctx.stroke ();
+    ctx.beginPath();
+    ctx.moveTo(pos.x, pos.y);
+    ctx.lineTo(newpos.x, newpos.y);
+    ctx.stroke();
   }
   pos = newpos;
 }
 
 function penUp() {
-  down=false;
+  log("penUp();");
+  down = false;
 }
 
 function penDown() {
-  down=true;
+  log("penDown();");
+  down = true;
 }
 
 function right(n) {
-  while (dir<0) {dir += 360;}
+  log("right(" + n + ");");
+  while (dir + n < 0) { dir += 360; }
   dir = (dir + n) % 360;
 }
 
@@ -59,6 +70,7 @@ function right(n) {
 // when deg < 0: left turn, deg > 0: right turn
 
 function turnRight(rad, deg) {
+  log("turnRight(" + rad + "," + deg + ");");
   var sgn = (deg < 0) ? -1 : 1;
   var corr = (deg < 0) ? 90 : 270;
   var cx = Math.round(pos.x - sgn * rad * sin(dir));
@@ -82,33 +94,46 @@ function turnRight(rad, deg) {
   }
   pos.x = newx;
   pos.y = newy;
-  while (dir + deg < 0) {dir += 360;}
+  while (dir + deg < 0) { dir += 360; }
   dir = (dir + deg) % 360;
 }
 
-function clearCanvas () {
-  ctx.clearRect (0, 0, 200, 200);
-  dir=0;
-  pos={x: 100, y: 100};
-  down=false;
+function clearCanvas() {
+  ctx.clearRect(0, 0, 200, 200);
+  dir = 0;
+  pos = {x: 100, y: 100};
+  down = false;
+  clearLog();
+}
+
+function repeat(n, f) {
+  var i;
+  for (i = 0; i < n; i += 1) {
+    f(i);
+  }
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 $(document).ready( function() {
 
   $("#forwardButton").on("click", function () {
-    forward(parseInt($("#forwardCount").val()));
+    forward(parseInt($("#forwardCount").val(), 10));
     displayState();
   });
 
   $("#rightButton").on("click", function () {
-    right(parseInt($("#rightCount").val()));
+    right(parseInt($("#rightCount").val(), 10));
     displayState();
   });
 
   $("#turnRightButton").on("click", function () {
     turnRight(
-      parseInt($("#turnRightRadius").val()),
-      parseInt($("#turnRightCount").val()));
+      parseInt($("#turnRightRadius").val(), 10),
+      parseInt($("#turnRightCount").val(), 10)
+    );
     displayState();
   });
 
@@ -142,7 +167,9 @@ $(document).ready( function() {
     displayState();
   });
 
-  function displayState () {
+  function displayState() {
+    pos.x = Math.round(pos.x);
+    pos.y = Math.round(pos.y);
     $("#xDisplay").val(pos.x);
     $("#yDisplay").val(pos.y);
     $("#dirDisplay").val(dir);
@@ -169,23 +196,19 @@ function rectangle(width, height) {
   alert("not yet defined");
 }
 
-function squareStep(size) {
-  square(size);
-  forward(size);
-}
-
-
 // functions linked to buttons:
 
 function testA() {
-  square50();
+  forward(50);
+  right(90);
 }
 
 function testB() {
-  turnRight(50, 45);
+  square50();
 }
 
 function testC() {
-
+  alert("not yet defined");
 }
+
 
